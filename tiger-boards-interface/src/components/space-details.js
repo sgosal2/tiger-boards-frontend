754457bb004d69c53e2140f0aebaecd02835b005
Max <maximomacchi@gmail.com>
2019-03-19 02:26:42 -0700

    Imported useState hook to track state of whether modal is opened or closed on SpaceDetails component

--- tiger-boards-interface/src/components/space-details.js
+++ tiger-boards-interface/src/components/space-details.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SpaceDetails = () => {
+  const { open, setOpen } = useState(false);
   return <div>Space Details</div>;
 };
 
