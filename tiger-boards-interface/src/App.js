efafbf4587e281395150ac70a62973b0cb324410
Nathan Chica <n_chica@u.pacific.edu>
2019-03-22 20:57:32 -0700

    setup basic switch and lazy loading

--- tiger-boards-interface/src/App.js
+++ tiger-boards-interface/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import { Typography, Paper } from "@material-ui/core";
 
 import "./scss/main.scss";
-import SpaceAvailabilityBoard from "./components/space-availability-board";
+// import SpaceAvailabilityBoard from "./components/space-availability-board";
 import { TigerBoardsAppBar } from "./components/tigerboards-appbar";
 import { LoginDialog } from "./components/login-dialog";
 
@@ -21,30 +22,44 @@ const theme = createMuiTheme({
   }
 });
 
+// Lazy load route components
+const spaceAvailabilityBoard = React.lazy(() =>
+  import("./components/space-availability-board")
+);
+const noMatch = React.lazy(() => import("./components/no-match"));
+
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
-        <TigerBoardsAppBar handleLoginClick={() => setShowLogin(true)} />
-        <LoginDialog
-          isOpen={showLogin}
-          handleClose={() => setShowLogin(false)}
-        />
+      <Router>
+        <div className="App">
+          <TigerBoardsAppBar handleLoginClick={() => setShowLogin(true)} />
+          <LoginDialog
+            isOpen={showLogin}
+            handleClose={() => setShowLogin(false)}
+          />
 
-        <div id="app-content-section">
-          {/* TODO: Change this to get current section title from state */}
-          <Typography align="left" variant="h2" id="view-header-text">
-            Space Availability
-          </Typography>
+          <div id="app-content-section">
+            {/* TODO: Change this to get current section title from state */}
+            <Typography align="left" variant="h2" id="view-header-text">
+              Space Availability
+            </Typography>
 
-          <Paper id="app-content">
-            {/* TODO: ReactRouter will go here */}
-            <SpaceAvailabilityBoard />
-          </Paper>
+            <Paper id="app-content">
+              {/* TODO: ReactRouter will go here */}
+              {/* <SpaceAvailabilityBoard /> */}
+              <Suspense fallback={<h1>Loading..</h1>}>
+                <Switch>
+                  <Route exact path="/" component={spaceAvailabilityBoard} />
+                  <Route component={noMatch} />
+                </Switch>
+              </Suspense>
+            </Paper>
+          </div>
         </div>
-      </div>
+      </Router>
     </MuiThemeProvider>
   );
 };
