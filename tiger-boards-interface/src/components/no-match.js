f544657032521305d0223dfb85cf7d0e0ac88249
Nathan Chica <n_chica@u.pacific.edu>
2019-03-31 12:19:51 -0700

    improved 404 page

--- tiger-boards-interface/src/components/no-match.js
+++ tiger-boards-interface/src/components/no-match.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 export const NoMatch = () => {
-  return <div>404</div>;
+  return (
+    <div id="no-match-content">
+      <Typography align="center" variant="h1">
+        404
+      </Typography>
+      <Typography align="center" variant="h5">
+        The page you are looking for can not be found.
+      </Typography>
+    </div>
+  );
 };
 
 export default NoMatch;
