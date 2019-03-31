1420ef963d982a0ee27d8c140ecb7aec572c1544
Nathan Chica <n_chica@u.pacific.edu>
2019-03-31 13:51:40 -0700

    started working on building manager

--- tiger-boards-interface/src/components/admin-view.js
+++ tiger-boards-interface/src/components/admin-view.js
@@ -1,20 +1,64 @@
 import React, { useState } from "react";
-import { Tabs, Tab } from "@material-ui/core";
+import {
+  Tabs,
+  Tab,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Button
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-export const AdminView = () => {
+// Don't use this.
+const dummyBuildings = ["Chambers", "Anderson", "Khoury"];
+
+const BuildingManager = () => {
+  const buildings = dummyBuildings;
+
+  return (
+    <div id="building-manager">
+      <Typography align="left" variant="h6">
+        Buildings
+      </Typography>
+      <List id="building-list">
+        {buildings.map(building => (
+          <ListItem key={building} dense button>
+            <ListItemText primary={building} />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+      <Button color="primary" id="add-new-building-btn">
+        Add New Building
+      </Button>
+    </div>
+  );
+};
+
+const AdminView = () => {
   const [currTab, setCurrTab] = useState(0);
 
   return (
-    <Tabs
-      value={currTab}
-      onChange={(event, value) => setCurrTab(value)}
-      indicatorColor="primary"
-      textColor="primary"
-      centered
-    >
-      <Tab label="Manage Spaces" />
-      <Tab label="Manage Admins" />
-    </Tabs>
+    <div id="admin-view-content">
+      <Tabs
+        value={currTab}
+        onChange={(event, value) => setCurrTab(value)}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Manage Spaces" />
+        <Tab label="Manage Admins" />
+      </Tabs>
+      <BuildingManager />
+    </div>
   );
 };
 
