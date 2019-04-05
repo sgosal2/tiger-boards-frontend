3b16eae124621c6ea0d4ea79574a9eff852d9db9
Max <maximomacchi@gmail.com>
2019-04-04 22:52:29 -0700

    Grid setup on AdminPage component

--- src/components/admin-page.js
+++ src/components/admin-page.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import AddSystemAdminForm from "./add-system-admin-form";
+import SystemAdminList from "./system-admin-list";
 
 export const AdminPage = () => {
-  return <div>Admin page</div>;
+  let admins = ["Max", "Sahib", "Nate"];
+  return (
+    <div>
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <SystemAdminList admins={admins} />
+        </Grid>
+        <Grid item xs={6}>
+          <AddSystemAdminForm />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
