8952c6aa5fa01fa9c3101c14bcebf350a9b885ff
Max <maximomacchi@gmail.com>
2019-04-04 23:38:08 -0700

    AddSystemAdminForm component now has form for adding new system admin

--- src/components/add-system-admin-form.js
+++ src/components/add-system-admin-form.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+import {
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+  Typography
+} from "@material-ui/core";
+
+let addSystemAdmin = admin => {
+  alert(admin);
+};
 
 export const AddSystemAdminForm = () => {
-  return <div>Add System Admin Here</div>;
+  const [emailToAdd, setEmailToAdd] = useState("");
+  return (
+    <div>
+      <Typography variant="h6">Add System Administrator</Typography>
+      <FormControl margin="none">
+        <InputLabel htmlFor="emailToAddInput">Email</InputLabel>
+        <Input
+          id="emailToAddInput"
+          name="emailToAddInput"
+          type="email"
+          required
+          onChange={e => setEmailToAdd(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          id="addSystemAdminSubmitButton"
+        >
+          Add
+        </Button>
+      </FormControl>
+    </div>
+  );
 };
+
+export default AddSystemAdminForm;
