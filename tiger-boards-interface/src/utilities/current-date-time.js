b37cce1cf76183495ce26ec7781e8b3d03d86b9f
Nathan Chica <n_chica@u.pacific.edu>
2019-04-08 17:13:35 -0700

    export current date string

--- tiger-boards-interface/src/utilities/current-date-time.js
+++ tiger-boards-interface/src/utilities/current-date-time.js
@@ -1,12 +1,19 @@
-const currentDateTime = () => {
-  /* Required format is yyyy-MM-ddThh:mm */
-
+export const currentDate = () => {
   const today = new Date();
   let month = today.getMonth() + 1;
   month = month >= 10 ? month : `0${month}`;
   let currDate = today.getDate();
   currDate = currDate >= 10 ? currDate : `0${currDate}`;
   const date = `${today.getFullYear()}-${month}-${currDate}`;
+
+  return date;
+};
+
+const currentDateTime = () => {
+  /* Required format is yyyy-MM-ddThh:mm */
+
+  const date = currentDate();
+  const today = new Date();
 
   let hour = today.getHours();
   hour = hour >= 10 ? hour : `0${hour}`;
