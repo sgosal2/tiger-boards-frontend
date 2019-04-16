09628df937be55df50389f2a83c582b2b8572336
Max <maximomacchi@gmail.com>
2019-04-16 00:38:15 -0700

    EmailSettings now renders form for adjusting email preferneces

--- src/components/email-settings.js
+++ src/components/email-settings.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Checkbox, FormControl, Typography } from "@material-ui/core";
 
-export const EmailSettings = () => {
-  return <div>EmailSettings</div>;
+export const EmailSettings = userEmail => {
+  const [emailDaily, setEmailDaily] = useState(false);
+  const [emailWeekly, setEmailWeekly] = useState(false);
+
+  return (
+    <div>
+      <Typography variant="h6">Email Preferences</Typography>
+      <FormControl margin="none">
+        <Typography variant="body1">Email daily</Typography>
+        <Checkbox
+          id="emailDaily"
+          onChange={e => setEmailDaily(e.target.checked)}
+        />
+        <Typography variant="body1">Email weekly</Typography>
+        <Checkbox
+          id="emailWeekly"
+          onChange={e => setEmailWeekly(e.target.checked)}
+        />
+        <Button color="primary" id="updateEmailPreferences">
+          Update email preferences
+        </Button>
+      </FormControl>
+    </div>
+  );
 };
 
 export default EmailSettings;
