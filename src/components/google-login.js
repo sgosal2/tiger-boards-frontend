5615e844c4aa985198bc670d8096902e27a3a308
Max <maximomacchi@gmail.com>
2019-05-02 21:37:52 -0700

    GoogleLogin component renders Log Out button if user is signed in. Clicking button logs user out

--- src/components/google-login.js
+++ src/components/google-login.js
@@ -1,21 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "@material-ui/core";
 
 const GOOGLE_BUTTON_ID = "google-sign-in-button";
-const renderLoginButton = () => {
-  window.gapi.signin2.render(GOOGLE_BUTTON_ID, {
-    width: 90,
-    height: 30,
-    scope: "email",
-    onsuccess: onSuccess
-  });
-};
-const onSuccess = googleUser => {
-  // Make API call here for JWT
-  const profile = googleUser.getBasicProfile();
-  console.log("Email: " + profile.getEmail());
-};
+let googleUserObj;
 
 export const GoogleLogin = () => {
+  const [loggedIn, changeLoggedIn] = useState(false);
+
+  const renderLoginButton = () => {
+    window.gapi.signin2.render(GOOGLE_BUTTON_ID, {
+      width: 90,
+      height: 30,
+      scope: "email",
+      onsuccess: onSuccess
+    });
+  };
+
+  const onSuccess = googleUser => {
+    // Make API call here for JWT
+    googleUserObj = googleUser;
+    const profile = googleUserObj.getBasicProfile();
+    console.log("Email: " + profile.getEmail());
+    changeLoggedIn(true);
+  };
+
+  const logOut = () => {
+    googleUserObj.disconnect().then(() => {
+      googleUserObj = null;
+      changeLoggedIn(false);
+    });
+  };
+
   useEffect(() => {
     try {
       renderLoginButton();
@@ -28,7 +43,15 @@ export const GoogleLogin = () => {
     }
   });
 
-  return <div id={GOOGLE_BUTTON_ID} />;
+  if (!loggedIn) {
+    return <div id={GOOGLE_BUTTON_ID} />;
+  } else {
+    return (
+      <Button onClick={() => logOut()} color="inherit">
+        Log Out
+      </Button>
+    );
+  }
 };
 
 export default GoogleLogin;
