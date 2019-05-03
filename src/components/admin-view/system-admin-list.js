d2e4178c518b6f242b49e43bed736bd3e27099d7
Max <maximomacchi@gmail.com>
2019-05-02 23:28:32 -0700

    SystemAdminList now fetches data from API for list of admins

--- src/components/admin-view/system-admin-list.js
+++ src/components/admin-view/system-admin-list.js
@@ -1,40 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Button,
+  CircularProgress,
   List,
   ListItem,
   ListItemText,
   ListItemSecondaryAction,
   Typography
 } from "@material-ui/core";
+import config from "../../config.json";
+import useDataApi from "../../utilities/use-data-api";
 
 let handleAdminRemove = admin => {
   alert(`${admin} will be removed from the admin list`);
 };
 
 export const SystemAdminList = admins => {
-  return (
-    <div>
-      <Typography variant="h6">System Administrators</Typography>
-      <List>
-        {admins.admins.map(admin => {
-          return (
-            <ListItem>
-              <ListItemText primary={admin} />
-              <ListItemSecondaryAction>
-                <Button
-                  color="primary"
-                  onClick={() => handleAdminRemove(admin)}
-                >
-                  Remove
-                </Button>
-              </ListItemSecondaryAction>
-            </ListItem>
-          );
-        })}
-      </List>
-    </div>
-  );
+  const systemAdmins = useDataApi({});
+  const updateSystemAdmins = useDataApi({});
+
+  const systemAdminsUrl = `${config.API_ADMINS}`;
+  useEffect(() => systemAdmins.doFetch(systemAdminsUrl), [systemAdminsUrl]);
+
+  if (systemAdmins.isLoading || systemAdmins.data.length == 0) {
+    return (
+      <div>
+        <Typography variant="h6">System Administrators</Typography>
+        <CircularProgress />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Typography variant="h6">System Administrators</Typography>
+        <List>
+          {systemAdmins.data.map(admin => {
+            return (
+              <ListItem>
+                <ListItemText primary={admin.email} />
+                <ListItemSecondaryAction>
+                  <Button
+                    color="primary"
+                    onClick={() => handleAdminRemove(admin.email)}
+                  >
+                    Remove
+                  </Button>
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
+      </div>
+    );
+  }
 };
 
 export default SystemAdminList;
